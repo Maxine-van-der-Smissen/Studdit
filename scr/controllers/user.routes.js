55571f4dd06fc6e17f1e6de010f4a42a0fe4e893
Maxime van der Smissen <m.smissen@outlook.com>
2019-11-21 23:00:36 +0100

    Add endpoints for friendships
    Friendships can now be created and deleted.

--- scr/controllers/user.routes.js
+++ scr/controllers/user.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const neo4j = require('neo4j-driver').v1;
+const driver = neo4j.driver("bolt://hobby-npbnlebfkikggbkepkdfkddl.dbs.graphenedb.com:24787", neo4j.auth.basic("dilivio", "b.x18kQlQ3Hq5Z.qzUPTD4X35ePsosx"));
+
 const User = require('../models/user.model');
 
 //Deprication safe settings for removing or updating using the model
@@ -25,7 +28,7 @@ router.put('/:username', (req, res) => {
             if (user) {
                 if (user.password === password) {
                     return user.updateOne({ password: newPassword })
-                    .then(() => res.status(200).send({ username: user.username }))
+                        .then(() => res.status(200).send({ username: user.username }))
                 }
                 else {
                     res.status(401).send({ error: 'Username and password didn\'t match!' });
@@ -35,7 +38,7 @@ router.put('/:username', (req, res) => {
                 res.status(204).send();
             }
         })
-        .catch (error => res.status(401).send({ error: error }));
+        .catch(error => res.status(401).send({ error: error }));
 });
 
 //Delete a user by setting the active flag to false
@@ -60,6 +63,48 @@ router.delete('/:username', (req, res) => {
             }
         })
         .catch(error => res.status(401).send({ error: error }));
+});
+
+router.post('/friends', (req, res) => {
+    const { befriender, befriended } = req.body;
+    const session = driver.session();
+
+    res.status(202).send();
+    User.find({ $or: [{ username: befriender }, { username: befriended }] })
+        .then(users => {
+            if (users.length !== 2) throw new Error();
+
+            return session.run(`
+                MERGE (befriender:User {username:"${befriender}"})
+                MERGE (befriended:User {username:"${befriended}"})
+                MERGE (befriender)-[:FRIENDS_WITH]->(befriended)
+            `);
+        })
+        .then(() => console.log('Relation was made!'))
+        .catch(console.log);
+
+    session.close();
+});
+
+router.delete('/friends/:befriender', (req, res) => {
+    const befriender = req.params.befriender;
+    const befriended = req.body.befriended;
+    const session = driver.session();
+
+    res.status(202).send();
+    User.find({ $or: [{ username: befriender }, { username: befriended }] })
+        .then(users => {
+            if (users.length !== 2) throw new Error();
+
+            return session.run(`
+                MATCH (:User {username:"${befriender}"})-[friendship:FRIENDS_WITH]->(:User {username:"${befriended}"})
+                DELETE friendship
+            `);
+        })
+        .then(() => console.log('Relation was destroyed!'))
+        .catch(console.log);
+
+    session.close();
 });
 
 module.exports = router;
